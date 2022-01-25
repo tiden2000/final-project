3dca2510422ec3d9b7a787719d2f030b6bb93eb9
tiden2000 <tiden2000@gmail.com>
2022-01-25 21:42:59 +0700

    a

--- config.php
+++ config.php
@@ -13,5 +13,5 @@
     );
 
     // Connection info
-    $conn = mysqli_connect('final-year-project-325215:asia-southeast1:heroku-project', 'root', 'Chienbot123', 'finalprojectdb', '3306'); // enter your info
+    $conn = mysqli_connect('27.3.0.248', 'root', '', 'finalprojectdb'); // enter your info
 ?>
