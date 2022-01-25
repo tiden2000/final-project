2714a2ea77ac43cc362ce40ca22677e1121efd96
tiden2000 <tiden2000@gmail.com>
2022-01-25 17:14:10 +0700

    A

--- config.php
+++ config.php
@@ -1,16 +1,4 @@
 <?php
-    // Blockonmics API
-    $apikey = "WAnsk4iUPjdHPsHX4wKmvWNXB2FjGtzFWWZ6dGtZWdc";
-    $url = "https://www.blockonomics.co/api/";
-    
-    $options = array( 
-        'http' => array(
-            'header'  => 'Authorization: Bearer '.$apikey,
-            'method'  => 'POST',
-            'content' => '',
-            'ignore_errors' => true
-        )   
-    );
 
     // Connection info
     $conn = mysqli_connect("final-year-project-325215:asia-southeast1:heroku-project", "root", "Chienbot123", "finalprojectdb"); // enter your info
